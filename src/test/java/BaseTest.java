0201c821b8c863499d22257e095adcfde3ee9310
Abraham Miranda <abraham.miranda.rodriguez@gmail.com>
2023-05-03 21:36:12 -0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/test/java/BaseTest.java
    #	src/test/java/GoogleTest.java
    #	target/test-classes/BaseTest.class
    #	target/test-classes/GoogleTest.class

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -11,4 +11,6 @@ public class BaseTest {
         webDriver = new FirefoxDriver();
         return webDriver;
     }
+
+
 }
--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,4 +1,3 @@
-import Steps.BaseSteps;
 import Steps.GoogleSteps;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -7,22 +6,23 @@ import org.testng.annotations.Test;
 public class GoogleTest extends BaseTest {
     private WebDriver webDriver = getWebDriver();
     private GoogleSteps googleSteps = new GoogleSteps(webDriver);
-    private BaseSteps baseSteps = new BaseSteps(webDriver);
+    //private BaseSteps baseSteps = new BaseSteps(webDriver);
 
     @Test
     public void openGoogleTest(){
         googleSteps.abrirPaginaGoogle();
-        String urlActual = baseSteps.getURLActual();
+        String urlActual = googleSteps.getURLActual();
         System.out.println("La URL actual es: " + urlActual);
         Assert.assertEquals(urlActual, "https://www.google.com/");
         //baseSteps.cerrarVentana();
-        baseSteps.finalizarWebDriver();
+        googleSteps.finalizarWebDriver();
     }
-    @Test
+    @Test //Ejercicio 2 - Tarea 3
+    //titulo, url, codigo fuente
     public void titleGoogleTest(){
         googleSteps.abrirPaginaGoogle();
-        String urlActual = baseSteps.getURLActual();
-        String tituloActual = baseSteps.getTituloActual();
+        String urlActual = googleSteps.getURLActual();
+        String tituloActual = googleSteps.getTituloActual();
         //String codigoFuenteActual = baseSteps.getCodigoFuente();
         System.out.println("La URL actual es: " + urlActual);
         System.out.println("La Titulo actual es: " + tituloActual);
@@ -30,7 +30,41 @@ public class GoogleTest extends BaseTest {
         Assert.assertEquals(urlActual, "https://www.google.com/");
         Assert.assertEquals(tituloActual, "Google");
         //baseSteps.cerrarVentana();
-        baseSteps.finalizarWebDriver();
+        googleSteps.finalizarWebDriver();
     }
-//titulo, url, codigo fuente
+    @Test //Trabajo clase 03/05/23
+    public void moveGoogleTest(){
+        //Google
+        String urlExpected = "https://www.google.com/";
+        googleSteps.abrirPaginaGoogle();
+        String urlActual = googleSteps.getURLActual();
+        googleSteps.imprimir(urlActual);
+        Assert.assertEquals(urlActual, urlExpected);
+
+        //Facebook
+        String getTituloFacebookExpected = "Facebook - log in or sign up";
+        googleSteps.navegarPaginaFacebook();
+        String getTituloFacebookActual = googleSteps.getTituloActual();
+        googleSteps.imprimir(getTituloFacebookActual);
+        Assert.assertEquals(getTituloFacebookActual, getTituloFacebookExpected);
+
+        //ESPN
+        String urlExpectedESPNExpected = "https://www.espn.com.mx/";
+        googleSteps.navegarPaginaESPN();
+        String urlActualESPNActual = googleSteps.getURLActual();
+        googleSteps.imprimir(urlActualESPNActual);
+        Assert.assertEquals(urlActualESPNActual, urlExpectedESPNExpected);
+
+        //Facebook
+        webDriver.navigate().back();
+        googleSteps.imprimir(googleSteps.getTituloActual());
+        Assert.assertEquals(googleSteps.getTituloActual(), getTituloFacebookExpected);
+
+        //Google
+        webDriver.navigate().back();
+        googleSteps.imprimir(googleSteps.getURLActual());
+        Assert.assertEquals(googleSteps.getURLActual(), urlExpected);
+
+        googleSteps.finalizarWebDriver();
+    }
 }
